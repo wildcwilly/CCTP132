bb7ccba69a2e34b378ce562a776adf45fada3b00
Chris Wild <wildcwilly@gmail.com>
2018-11-16 14:19:17 -0700

    write expression using () for precedence

--- Chapter 1/1_4_40_1.cpp
+++ Chapter 1/1_4_40_1.cpp
@@ -4,7 +4,7 @@ Chris Wild
 Scenario
 Take a look at the code below: it reads a float value, puts it into a variable named x and prints the value of a variable named y. Your task
 is to complete the code in order to evaluate the following expression:
-    see document
+    y = x^2 / π^2 (x^2 + 1/2) * (1 + x^2/π^2(x^2--1/2)^2)
 We expect the result to be assigned to y.
 Note: we've prepared a variable containing the value of π. Use it.
 Be careful! Watch the operators and keep their priorities in mind. Remember that classical algebraic notation likes to omit the
@@ -21,9 +21,12 @@ using namespace std;
     int main(void) {
     float pi = 3.14159265359;
     float x,y;
+
     cout << "Enter value for x: ";
     cin >> x;
+
     // put your code here
+
     cout << "y = " << y;
     return 0;
 }
@@ -59,10 +62,30 @@ using namespace std;
 
     int main(void) {
     float pi = 3.14159265359;
-    float x,y;
+    float x,y, sqrX, sqrPi;
+
     cout << "Enter value for x: ";
     cin >> x;
-    // put your code here
-    cout << "y = " << y;
+
+    // calculate square values to clean up calculation
+    sqrPi = pi * pi;
+    sqrX = x * x;
+
+    //  y = x^2 / π^2 (x^2 + 1/2) * (1 + x^2/π^2(x^2--1/2)^2)
+    y = sqrX / (sqrPi * (sqrX + (float)1/2)) * (1 + sqrX / (sqrPi * ((sqrX - (float)1/2) * (sqrX - (float)1/2))));
+
+    cout << "y = " << y << endl;
     return 0;
 }
+
+/* Actual output (copied from console window)
+
+Enter value for x: 1
+y = 0.0949234
+
+Enter value for x: -1.5
+y = 0.0890702
+
+Enter value for x: 12.345
+y = 0.101057
+*/
