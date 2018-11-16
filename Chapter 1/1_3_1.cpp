bb7dfb960d8833ddeed24c6d1ee5f2e78423901b
Chris Wild <wildcwilly@gmail.com>
2018-11-16 13:14:20 -0700

    rename 1.2.1.cpp to  1_2_1.cpp add 1_3_1.cpp with lab instructions

---
+++ Chapter 1/1_3_1.cpp
@@ -0,0 +1,39 @@
+/* 1.3.1 Comments: are they always useful?
+Chris Wild
+
+Comments are not always the best way to say something in code. Sometimes it's much better to leave some information in the code. In
+the worst-case scenario, comments can lie to the user/programmer (you should never do that on purpose). It's good to use readable
+variable names, and sometimes it's better to divide your code into named pieces (later we'll call these pieces functions). In some
+situations, it's a good idea to write the steps of computations in a clearer way. The code below contains some of these situations. Try to
+improve it (and remove the comments – removing the comments will sometimes be an improvement itself).
+
+#include <iostream>//we included iostream
+#include <iomanip>//we included iomanip
+#include <string>//we included string
+
+int main()
+{
+    int v=10800; // 3*60*60
+    int zzz=3*60;// This is a variable to hold the value of 3 minutes in seconds
+    int zzz=5*60;// This is a variable to hold the value of 6 minutes in seconds
+    float siii=3.141526; //This is the value of pi
+    //std::cout << "result: " << result << std::endl;
+    // here we should print the v value but a programmer didn't have time to write any code
+    // but he/she likes writing long comments
+}
+*/
+
+#include <iostream>//we included iostream
+#include <iomanip>//we included iomanip
+#include <string>//we included string
+
+int main()
+{
+    int v=10800; // 3*60*60
+    int zzz=3*60;// This is a variable to hold the value of 3 minutes in seconds
+    int zzz=5*60;// This is a variable to hold the value of 6 minutes in seconds
+    float siii=3.141526; //This is the value of pi
+    //std::cout << "result: " << result << std::endl;
+    // here we should print the v value but a programmer didn't have time to write any code
+    // but he/she likes writing long comments
+}
