97e2bc6763c992717a1b7b2c7dca2f6c081b85ad
Chris Wild <wildcwilly@gmail.com>
2018-11-16 12:48:57 -0700

    add a greeting

--- Chapter 1/1.2.1.cpp
+++ Chapter 1/1.2.1.cpp
@@ -34,6 +34,35 @@ using namespace std;
 
 int main(void)
 {
-    cout << "It's me, your first program.";
+    cout << "Hello Chris, I'm your first program.\n";
     return 0;
 }
+
+/* results
+* add a greeting – let the program welcome you in a warm and pleasant way;
+
+    Hello Chris, I'm your first program.
+
+* duplicate (or triplicate) the greeting to welcome more than one person;
+    
+
+* insert a line saying cout << endl; between two other couts and check the effects it has; does it look interesting? You're going to
+  learn more about it soon;
+
+
+* now try to insert a mysteriously-looking sequence of chars into any of the greeting: \n – there are exactly two characters: a
+  backslash and lower-case n; what happens now?;
+
+
+* try to remove any of the semicolons and look carefully at the compiler's response; pay attention to where the compiler sees an
+  error – is this where the error really is?
+
+
+* change the name of the main function to any other lexically correct word (e.g. Main); what happens now? Can you explain the
+  result?
+
+
+* remove some of the quotes (opening and closing ones respectively); does the compiler like that? What does it think of your
+  actions?
+
+*/
