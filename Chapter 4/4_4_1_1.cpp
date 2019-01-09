a2c1a9205b6163d6d5a53f21c9abd463c896120d
Chris Wild <wildcwilly@gmail.com>
2019-01-09 15:18:36 -0700

    add 4_4_1_1.cpp in progress

---
+++ Chapter 4/4_4_1_1.cpp
@@ -0,0 +1,92 @@
+/* 4.4.1 String manipulation: pangrams
+Chris Wild
+
+A pangram is a sentence that uses every letter of the alphabet at least once. The most popular English pangram is "The quick brown fox
+jumps over the lazy dog." It contains 35 letters and each letter is used at least once. Write code to check whether a given string is a
+pangram or not. Try to write the shortest code possible. You can use the string class and arrays. Remember not to count spaces and
+other special characters. At the end of this program you should print how many times each letter has been used.
+
+Example input
+
+The quick brown fox jumps over the lazy dog
+
+Example output
+
+Pangram
+a-1
+b-1
+c-1
+d-1
+e-3
+f-1
+g-1
+h-2
+i-1
+j-1
+k-1
+l-1
+m-1
+n-1
+o-4
+p-1
+q-1
+r-2
+s-1
+t-2
+u-2
+w-1
+v-1
+x-1
+y-1
+z-1
+
+Example input
+
+quick brown fox jumps over lazy dog
+
+Example output
+
+Not pangram
+a-1
+b-1
+c-1
+d-1
+e-1
+f-1
+g-1
+h-0
+i-1
+j-1
+k-1
+l-1
+m-1
+n-1
+o-4
+p-1
+q-1
+r-2
+s-1
+t-0
+u-2
+w-1
+v-1
+x-1
+y-1
+z-1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
+
+/* Actual output (copied from console window)
+
+
+*/
