0c95f69cb24c8e12fdd8784598089bd7f93a638e
Chris Wild <wildcwilly@gmail.com>
2019-01-11 11:30:43 -0700

    complete 4_5_7_1.cpp lab with sample output

--- Chapter 4/4_5_7_1.cpp
+++ Chapter 4/4_5_7_1.cpp
@@ -54,11 +54,48 @@ using namespace std;
 int main()
 {
 	string sentence;
+	int currentStart = 0, currentEnd, searchStart, searchEnd;
 
 	cout << "Type a sentence: ";
 	getline(cin, sentence);
 
-	// manipulate the sentence here
+	// iterate through each word to search for duplicates in sentence
+	do
+	{
+		// current word is from currentStart to currentEnd in sentence
+		currentEnd = sentence.find(" ", currentStart);
+
+		searchStart = currentEnd + 1;	// start search for duplicates after current word
+
+		if (currentEnd != string::npos)	// no need to search the last word
+		{
+			// iterate through remaining words to compare with current word
+			do
+			{
+				searchEnd = sentence.find(" ", searchStart);
+
+				// check if just a duplicate space
+				if (searchStart == searchEnd)
+				{
+					sentence.erase(searchStart, 1);
+				}
+
+				// compare both words
+				if (sentence.substr(currentStart, currentEnd - currentStart).compare(sentence.substr(searchStart, searchEnd - searchStart)))
+				{
+					// not equal
+					searchStart = searchEnd + 1; // move search word to next word
+				}
+				else
+				{
+					// both words are equal, delete search word
+					sentence.erase(searchStart, (searchEnd)-searchStart);
+				}
+			} while (searchEnd != string::npos);
+		}
+
+		currentStart = currentEnd + 1;	// move start to next word;
+	} while (currentEnd != string::npos);
 
 	cout << sentence << "\n";
 
@@ -68,5 +105,22 @@ int main()
 
 /* Actual output (copied from console window)
 
+Type a sentence: This sentence has no duplicate words
+This sentence has no duplicate words
 
+Type a sentence: This sentence has has only unique words
+This sentence has only unique words
+
+Type a sentence: You should, should you be interested, look for for more informa
+tion online
+You should, should you be interested, look for more information online
+
+Type a sentence: The old old brown cow cow!
+The old brown cow cow!
+
+Type a sentence: The old, old brown cow cow
+The old, old brown cow
+
+Type a sentence: The old brown cow!
+The old brown cow!
 */
